51dcec034eb26461b6ab80ba7c32cdab4aeeef85
ahmed sahal <a.yusuf2997@gmail.com>
2021-08-26 17:28:44 +0100

    test5

---
+++ test.py
@@ -0,0 +1,14 @@
+magaca = input("Magacaa? ")
+
+
+Dadaa = input("imisa jir ayaad tahay? ")
+dadaint = int (Dadaa)
+
+print("Soo dhowoow " + magaca)
+
+if (dadaint >= 18):
+    print ("Nin weyn")
+else:
+    print("caruur")
+
+
